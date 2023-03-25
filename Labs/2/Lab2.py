27043f3da72a9316bd1ce139499d858078a33a15
NemoNology <chicken.gang@mail.ru>
2023-03-25 12:55:39 +0900

    Build 1.0

--- Labs/2/Lab2.py
+++ Labs/2/Lab2.py
@@ -4,7 +4,6 @@ import numpy as np
 import seaborn as sb
 # import pandas as pd
 import matplotlib.pyplot as pl
-import math
 
 
 def task1(N: int = 3, M: int = 7, isRandIntMatrix: bool = False):
@@ -15,17 +14,24 @@ def task1(N: int = 3, M: int = 7, isRandIntMatrix: bool = False):
         M (int, optional): Columns amount. Defaults to 7.
     """
 
+    # uniform - floar matrix; randint - integer
+
     if isRandIntMatrix:
 
         return np.random.randint(-10, 10, (N, M))
 
     else:
 
+        
         return np.random.uniform(-2, 2, (N, M))
 
 
 def task2_test():
 
+    # СЛАУ - [a00 * x0, ... a0n * xn, b0] 
+    # СЛАУ - [....   . . ... . .. ..... ] 
+    # СЛАУ - [am0 * x0, ... amn * xn, bm] 
+    # Last Column - B
     m = np.array([
         [0.42, -0.72, 0.02, -0.28, 0.2, 0.66],
         [-0.67, 0.17, -0.86, 0.88, -0.88, 0.53],
@@ -45,23 +51,38 @@ def task2_test():
 
 def task3(matrix2D: np.ndarray):
 
+    # annot: if True - show matrix values; False - otherside
+    # cmap - using color palette  
     sb.heatmap(matrix2D, annot=True, cmap='binary')
     pl.show()
 
 
 def task4(matrix2D: np.ndarray):
 
+    # ndarray.ravel() - convert matrix to vector
     sb.histplot(matrix2D.ravel())
     pl.show()
 
 
-def task5(function=lambda x: (math.sin(x) - math.tan(x) * math.log(x)) * math.cbrt(x**-1) + math.sqrt(x) * x):
+def task5(function=lambda x: (np.sin(x) - np.tan(x) * np.log(x)) * np.cbrt(x**-1) + np.sqrt(x) * x):
 
-    rng = list(range(1, 100))
-    data = [function(x / 10) for x in rng]
+    # np.arange is usual range, but with float type possibility 
+    Xs = np.arange(1, 100, 0.1)
+    Ys = function(Xs)
+    Ys_noise = Ys + np.random.uniform(-30, 30, (len(Xs)))
 
-    pl.plot(data)
+    # xlabel - name of horisontal Axis; ylabel - vertical
+    # label - name of plot
+    # pl.legend() - Enable legends
+    pl.xlabel('X')
+    pl.ylabel('Y')
+    pl.plot(Ys, label='(sin(x) - tan(x * log(x)) * (x^-1)^(1/3) + sqrt(x) * x')
+    pl.plot(Ys_noise, label='(sin(x) - tan(x * log(x)) * (x^-1)^(1/3) + sqrt(x) * x - with Noise', alpha=0.4)
+    pl.subplot()
+    pl.legend()
+    pl.grid(True)
     pl.show()
 
 
-task2_test()
+
+task5()
