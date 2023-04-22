7585762a66b854e0f5862d0e0188fe2c9e20a69e
NemoNology <chicken.gang@mail.ru>
2023-04-23 04:35:56 +0900

    Build 1.1

--- Labs/7/Project/MainWindow.xaml.cs
+++ Labs/7/Project/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
@@ -16,29 +17,21 @@ namespace Project
             InitializeComponent();
 
             _gameLife.Changes += RedrawImage;
-            _gameLife.RandomFilling();
 
-            _gameSimulationTask = Task.Run(async () =>
-            {
-                while (true)
-                {
-                    if (_isSimulationAlive)
-                    {
-                        _gameLife.Move();
+            mainView.Source = BitmapToImageSource(_mainViewBuffer);
 
-                        await Task.Delay(600);
-                    }
-                }
-            });
+            Task.Factory.StartNew(GameSimulate);
         }
 
         private const int GameSize = 20;
 
-        private GameLife _gameLife = new GameLife(GameSize);
-        private Bitmap _mainViewBuffer = new Bitmap(GameSize, GameSize);
+        private readonly GameLife _gameLife = new(GameSize);
+        private readonly Bitmap _mainViewBuffer = new(GameSize, GameSize);
 
-        private readonly Task _gameSimulationTask;
         private bool _isSimulationAlive = false;
+
+        // For locking
+        private readonly object _mutex = new();
 
         /// <summary>
         /// Convert Bitmap to ImageSource <b> [Not Mine] </b>
@@ -47,9 +40,9 @@ namespace Project
         /// <returns> Converted Bitmap as ImageSource </returns>
         private ImageSource BitmapToImageSource(Bitmap bitmap)
         {
-            MemoryStream ms = new MemoryStream();
+            MemoryStream ms = new ();
             bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-            BitmapImage image = new BitmapImage();
+            BitmapImage image = new ();
             image.BeginInit();
             ms.Seek(0, SeekOrigin.Begin);
             image.StreamSource = ms;
@@ -62,7 +55,7 @@ namespace Project
         /// Redraw main view by updated cells values
         /// </summary>
         /// <param name="changes"></param>
-        private void RedrawImage(List<(int x, int y, bool newCellValue)> changes)
+        private async void RedrawImage(List<(int x, int y, bool newCellValue)> changes)
         {
             var liveCellColor = Color.White;
             var deadCellColor = Color.Black;
@@ -73,8 +66,78 @@ namespace Project
                     change.newCellValue ? liveCellColor : deadCellColor);
             }
 
-            mainView.Source = BitmapToImageSource(_mainViewBuffer);
+            
+            await Dispatcher.InvokeAsync(() =>
+            {
+                mainView.Source = BitmapToImageSource(_mainViewBuffer);
+            });
         }
 
+        private void RandomizeGameZone_Click(object sender, RoutedEventArgs e)
+        {
+            _gameLife.RandomFilling();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            lock (_mutex)
+            {
+                _isSimulationAlive = false;
+            }
+        }
+
+        private void Continue_Click(object sender, RoutedEventArgs e)
+        {
+            lock (_mutex)
+            {
+                _isSimulationAlive = true;
+            }
+        }
+
+        /// <summary>
+        /// Additional thread algorithm - game simulation
+        /// </summary>
+        private async void GameSimulate()
+        {
+            while (true)
+            {
+                if (_isSimulationAlive)
+                {
+                    lock (_mutex)
+                    {
+                        _gameLife.Move();
+                    }
+                }
+
+                await Task.Delay(600);
+            }
+        }
+
+        private void MainView_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var coordinates = e.GetPosition(sender as IInputElement);
+
+            var x = Math.Round(coordinates.X * GameSize / mainView.ActualWidth, 
+                MidpointRounding.ToPositiveInfinity);
+            var y = Math.Round(coordinates.Y * GameSize / mainView.ActualHeight,
+                MidpointRounding.ToPositiveInfinity);
+
+            outputMouseCoordinates.Content = $"{x}, {y}";
+        }
+
+        private void MainView_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var coordinates = e.GetPosition(sender as IInputElement);
+
+            var x = Math.Round(coordinates.X * GameSize / mainView.ActualWidth,
+                MidpointRounding.ToPositiveInfinity);
+            var y = Math.Round(coordinates.Y * GameSize / mainView.ActualHeight,
+                MidpointRounding.ToPositiveInfinity);
+
+            lock (_mutex)
+            {
+                _gameLife.ChangeStateForCell((int)x, (int)y);
+            }
+        }
     }
 }
